bfa9cfb1368994059608e3da40c8b03cd8050ac8
Jyotika Kishor <jkishor1802@gmail.com>
2025-06-27 12:09:48 +0530

    26-06-25

---
+++ Matrix01.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/01-matrix/
+
+class Matrix01 {
+
+    public int[][] updateMatrix(int[][] mat) {
+        int rows = mat.length;
+        int cols = mat[0].length;
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = mat[i][j] == 0 ? 0 : Integer.MAX_VALUE - 10000;
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (i > 0) {
+                    result[i][j] = Math.min(result[i][j], result[i - 1][j] + 1);
+                }
+                if (j > 0) {
+                    result[i][j] = Math.min(result[i][j], result[i][j - 1] + 1);
+                }
+            }
+        }
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 0; j--) {
+                if (i < rows - 1) {
+                    result[i][j] = Math.min(result[i][j], result[i + 1][j] + 1);
+                }
+                if (j < cols - 1) {
+                    result[i][j] = Math.min(result[i][j], result[i][j + 1] + 1);
+                }
+            }
+        }
+
+        return result;
+    }
+}
